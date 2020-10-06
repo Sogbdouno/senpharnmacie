c7bedb5c524ab3db22b61432b1d3f349ad0b7c96
Macbook Air <boubadiop@192.168.1.5>
2020-10-06 20:13:50 +0000

    enregistrement des applications medicament pharmacie et account

--- senpharmacie/settings.py
+++ senpharmacie/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pharmacie',
+    'pharmacie.apps.PharmacieConfig',
+    'medicament.apps.MedicamentConfig'
+    'account.apps.AccountConfig',
 
 ]
 
